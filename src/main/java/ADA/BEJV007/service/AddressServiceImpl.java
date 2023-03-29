1262ee37d5333320982f449cffa50c02085fd152
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-03-29 18:53:19 -0300

    resources template home

--- src/main/java/ADA/BEJV007/service/AddressServiceImpl.java
+++ src/main/java/ADA/BEJV007/service/AddressServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class AddressServiceImpl implements AddressService {
+public class AddressServiceImpl implements org.example.service.AddressService {
 
     private final AddressRepository repository;
 
