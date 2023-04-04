af1959267153a7e0f14f9906f61c2a4d246adb43
Thomas Ramiro <116609321+ThomRamks@users.noreply.github.com>
2023-04-04 19:41:39 -0300

    Acertando métodos da Adoção

--- src/main/java/ADA/BEJV007/domain/Pet.java
+++ src/main/java/ADA/BEJV007/domain/Pet.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -22,12 +23,17 @@ public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "É preciso inserir um nome")
     private String nome;
+    @NotNull(message = "É preciso inserir um tipo de animal")
     @Enumerated(EnumType.STRING)
     private TiposPet tipo;
     private LocalDate nascimento;
+    @NotBlank(message = "Insira uma descrição do animal (cor, pelagem, raça...)")
     private String descricao;
+    @NotNull(message = "É preciso inserir um status ao animal")
     @Enumerated(EnumType.STRING)
     private StatusPet status;
+    @NotBlank(message = "É preciso inserir uma imagem")
     private String linkImagem;
 }
